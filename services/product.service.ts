e02d3d5ed0a09fbdabf6b3fdd66e97a5ede00e6e
zahra shahab <zahrashahab@zahras-MacBook-Pro.local>
2024-11-05 16:27:52 +0100

    change port of api

--- services/product.service.ts
+++ services/product.service.ts
@@ -1,6 +1,6 @@
 import { IProduct } from "@/interfaces/product";
 
-const url = "http://localhost:5002";
+const url = "http://localhost:5000";
 
 export const getAllProduct = async (): Promise<IProduct[]> =>{
     const data = await fetch(`${url}/public`);
